882e44d0a485b5b598c1040274644535ddeb988e
Lillac <30539721+lillac@users.noreply.github.com>
2017-08-06 08:47:40 +1200

    Create part1.py

---
+++ part1.py
@@ -0,0 +1,7 @@
+print("A Moment In Time")
+print("A moment in time, just one small, simplistic second; can change your life entirely. Memories so vivid, as if it was really true. Sometimes, those moments are entirely left beyond words. You can try to describe it, terrifying? maybe.  If only it was within reach, to feel that fraction of a second again. The light fading away, the heavy weight of the ocean above you. How your lungs slowly collapsed from pressure. Strangely calm, a moment of peace. Content. As if the world had frozen; for just for a moment in time, I felt at ease with the universe.\nJust one moment, that was enough...")
+a = input("\nYou feel the intense pressure of the water above you, how far? 100meteres? Maybe. You struggle to fight your way to the surface but it's no good. It was as if the water has you by the limbs. You should probably still try, right? y/n")
+if a == "y":
+  print("You tug your hands away from the oceans steady grip, reaching towards the light, you legs still dangling uselessly behind you. The light grows storonger, until it's buring your eyes. Am I close yet? black.")
+else:
+  print("You give up on trying. It's useless anyway. I could feel the pain as my organs were pushing inwards. It's incredible I have lasted this long. I feel the ocean floor as I floated down seeminglessly. My back numb, I couldnt feel the rocks and coral under me. I shut my eyes.. black.")
